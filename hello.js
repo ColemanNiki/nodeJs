cd15380ccb09ed148ca92a3da563bc4dd98e30e6
ColemanNiki <nitanmao@qq.com>
2017-01-05 10:28:19 +0800

    监听

--- hello.js
+++ hello.js
@@ -1,7 +1,10 @@
 var http = require('http');
 http.createServer(function(require,response){
     response.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
-    console.log('访问');
-    response.write('hello world');
+    if(request.url !== "/favicon.ico"){
+        console.log('come on');
+        response.write('hello world');
+        response.end();
+    } 
 }).listen(8100);
 console.log('Server running at http://127.0.01.8000');
