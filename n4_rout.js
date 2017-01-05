9c1da962ef0e3c0520d23f91e1e28c64edfe34d8
ColemanNiki <nitanmao@qq.com>
2017-01-05 14:25:31 +0800

    rout

---
+++ n4_rout.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var url = require('url');
+http.createServer(function(request,response){
+    response.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
+    if(request.url !== "/favicon.ico"){
+        var pathname = url.parse(request.url).pathname;
+        // console.log('come on');
+        console.log(pathname);
+        response.end('');
+    } 
+}).listen(8100);
+console.log('Server running at http://127.0.01.8000');
