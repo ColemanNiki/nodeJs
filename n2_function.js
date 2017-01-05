9ed7c4b8d0c96559dba306cdb0b3396a2298d7d3
ColemanNiki <nitanmao@qq.com>
2017-01-05 12:05:40 +0800

    fff

--- n2_function.js
+++ n2_function.js
@@ -1,8 +1,10 @@
 var http = require('http');
+var otherfun = require("./models/otherfun.js");
 http.createServer(function(request,response){
     response.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
     if(request.url !== "/favicon.ico"){
         fun1(response);
+        fun2(response);
         response.end('');
     } 
 }).listen(8100);
@@ -11,4 +13,5 @@ console.log('Server running at http://127.0.01.8000');
 function fun1(res){
     console.log("fun1");
     res.write("hello,我是fun1");
+    
 }
