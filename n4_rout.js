4a89b0466a87a1c0474fb7e5364386a36c7bdec6
ColemanNiki <nitanmao@qq.com>
2017-01-05 15:28:56 +0800

    路由

--- n4_rout.js
+++ n4_rout.js
@@ -1,11 +1,14 @@
 var http = require('http');
 var url = require('url');
+var router = require('./router');
 http.createServer(function(request,response){
     response.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
     if(request.url !== "/favicon.ico"){
         var pathname = url.parse(request.url).pathname;
         // console.log('come on');
-        console.log(pathname);
+        pathname = pathname.replace(/\//,'');
+        // console.log(pathname);
+        router[pathname](request,response);
         response.end('');
     } 
 }).listen(8100);
