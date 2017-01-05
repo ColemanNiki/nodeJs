91b65181104fc20c316e561b7a9fb649b7da39c3
ColemanNiki <nitanmao@qq.com>
2017-01-05 12:41:08 +0800

    fff

--- models/otherfun.js
+++ models/otherfun.js
@@ -1,5 +1,10 @@
-function fun2(res){
-    console.log("我就是fun2");
+module.exports = {
+    fun2:function (res) {
+         console.log("我就是fun2");
     res.write("hello, fun2");
+    },
+    fun3:function(res){
+        console.log("this is fun3");
+        res.write("this is fun3");
+    }
 }
-module.exports = fun2;
