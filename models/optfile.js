8ff1ad81790604427441cd94cb368fb17ad77ca0
ColemanNiki <nitanmao@qq.com>
2017-01-05 17:16:04 +0800

    ff

---
+++ models/optfile.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+module.exports={
+    readfile:function(path){
+        fs.readfile(path,function(err,data){
+            if(err){
+                console.log(err);
+            }else{
+                console.log(data.toString());
+            }
+        });
+        console.log("异步方法执行完毕");
+    },
+    readfileSync:function(path){
+        var data = fs.readFileSync(path,'utf-8');
+        console.log("同步方法执行完毕");
+        return data;
+    }
+}
--- n5_readfile.js
+++ n5_readfile.js
@@ -3,7 +3,7 @@ var optfile = require('./models/optfile.js');
 http.createServer(function(request,response){
     response.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
     if(request.url !== "/favicon.ico"){
-        optfile.readfileSync('./views/login.html');
+        optfile['readfileSync']('./views/login.html');
         response.end('');
         console.log('主程序完毕');
     } 
