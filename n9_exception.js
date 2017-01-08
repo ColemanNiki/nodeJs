abc45398d8b18acb1597c31c15a77d99476a6d84
ColemanNiki <nitanmao@qq.com>
2017-01-08 19:29:40 +0800

    ff

--- n9_exception.js
+++ n9_exception.js
@@ -12,7 +12,7 @@ http.createServer(function(request,response){
         catch(err){
             console.log(''+err);
             response.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
-            response.write(err);
+            response.write(err.toString());
             response.end('');
         }
         console.log('主程序完毕');
