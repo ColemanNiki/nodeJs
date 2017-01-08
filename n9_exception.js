383ac06e8d32c5a89cd3195feebfe03afd97b7ae
ColemanNiki <nitanmao@qq.com>
2017-01-08 18:41:59 +0800

    错误处理

---
+++ n9_exception.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var router = require('./router');
+var url = require('url');
+var exception = require('./models/exception');
+http.createServer(function(request,response){
+    if(request.url !== "/favicon.ico"){
+        var pathname = url.parse(request.url).pathname;
+        pathname = pathname.replace(/\//,'');
+        try{
+            router[pathname](request,response);
+        }
+        catch(err){
+            console.log(''+err);
+            response.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
+            response.write(err);
+            response.end('');
+        }
+        console.log('主程序完毕');
+    } 
+}).listen(8100);
+console.log('Server running at http://127.0.01.8100');
